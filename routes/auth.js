e1109918f040a09c71a1b6453d58c9379cf5f93c
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2022-08-28 14:30:06 +0000

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,17 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 
-// router.use( (request, response, next) =>{
-//     console.log(request.sessionID);
-//     if(request.user) next()
-//     else {
-//         console.log("here");
-//         response.sendStatus(401)
-//     }
-// })
+router.use( (request, response, next) =>{
+    console.log(request.sessionID);
+    if(request.user) next()
+    else {
+        console.log("here");
+        response.sendStatus(401)
+    }
+})
 
 router.get('/', (request, response) => {
     console.log(request.user);
-    response.send(true)
+    response.send(request.user)
 })
 module.exports = router
