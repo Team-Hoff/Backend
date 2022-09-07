88078034bbaa725f5c3b4be9dd94425b26c9edcb
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2022-09-07 02:01:35 +0000

    Update logout.js

--- routes/logout.js
+++ routes/logout.js
@@ -3,13 +3,13 @@ const router = express.Router();
 
 const db = require('../models/database')
 
-router.use( (request, response, next) =>{
-    if(request.user) next()
-    else {
-        console.log("here");
-        response.sendStatus(401)
-    }
-})
+// router.use( (request, response, next) =>{
+//     if(request.user) next()
+//     else {
+//         console.log("here");
+//         response.sendStatus(401)
+//     }
+// })
 
 router.get("/", async (req, res) => {
     const sql="DELETE FROM sessions WHERE session_id = ?"
