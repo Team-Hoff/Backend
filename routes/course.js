71fc7ea71735a0fd21642ab5481b576e135318e0
LorenzoAppiadu <92297186+LorenzoAppiadu@users.noreply.github.com>
2022-09-18 23:26:00 +0000

    Update course.js

--- routes/course.js
+++ routes/course.js
@@ -6,10 +6,20 @@ const db = require('../models/database')
 
 router.get('/:programme', async(request,response,) =>{
     const {programme} = request.params;
-    let sql = `SELECT name,IDM,id,year,semester FROM  CourseInfo WHERE IDM = ${programme}`;
-    const result = await db.promise().query(sql);
+    let sql = `SELECT name,IDM,id,year,semester FROM  CourseInfo WHERE IDM = ?`;
+    const result = await db.promise().query(sql, [programme]);
     //console.log(result);
     response.send(result[0]);
      
 });
+
+router.get('/:programme/:course', async(request, response) => {
+    const {programme, course} = request.params;
+    let sql = `SELECT name,id,slides,ext,img FROM  CourseInfo WHERE IDM = ? AND name=?`;
+    const result = await db.promise().query(sql, [programme,course]);
+
+    response.send(result[0]);
+
+
+})
 module.exports=router;
