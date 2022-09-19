0ed31b9abc932a016bcccd6fff2bd3d32f9c9613
Kingbuffer <108902222+Kingbuffer@users.noreply.github.com>
2022-09-19 16:48:35 +0000

    oauth2 addition

---
+++ routes/oauth2/Strategies/keys.js
@@ -0,0 +1,9 @@
+//add it to gitignore
+module.exports={
+    google:{
+        clientID:'870205006030-5rmjbgd0ds7mo0b7obm7puhugutbom1q.apps.googleusercontent.com',
+        clientSecret:'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',
+        
+
+    }
+}
