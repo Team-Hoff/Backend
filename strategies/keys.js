0914e1ff3ec552367268bd7c3a5e5ffec5aa4abf
Kingbuffer <108902222+Kingbuffer@users.noreply.github.com>
2022-09-28 09:30:33 +0000

    Add telecom and reset keys

--- strategies/keys.js
+++ strategies/keys.js
@@ -2,7 +2,7 @@
 module.exports={
     google:{
         clientID:'870205006030-5rmjbgd0ds7mo0b7obm7puhugutbom1q.apps.googleusercontent.com',
-        clientSecret:'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',
+        clientSecret:'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',
         
 
     }
